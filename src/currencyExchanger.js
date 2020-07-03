bda7ed885bcfc4215967689264c5df5b91d726ec
Ian Gregg <iangregg188@gmail.com>
2020-07-03 13:56:20 -0700

    working on the UI and BI logic.

--- src/currencyExchanger.js
+++ src/currencyExchanger.js
@@ -1,6 +1,6 @@
 export async function getExchangeRate() {
   try {
-    let response = await fetch(`https://bikeindex.org:443/api/v3/search/count?location=${city}&distance=10&stolenness=stolen&appid=${process.env.API_KEY}`);
+    let response = await fetch(`${process.env.API_KEY}`);
     let currencyResponse;
     if (response.ok && response.status == 200) {
       currencyResponse = await response.json();
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
+import './currencyEchanger.js'
 
 $(document).ready(function() {
   event.preventDefault();
